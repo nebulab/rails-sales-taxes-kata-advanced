845b5e1037952dbe5529b59fa47076a2411e980c
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-11 11:33:59 -0600

    Update baskets test to test authorized user access
    Tests ensure that an unauthorized user cannot access the basket page or
    see other user's baskets.

--- spec/features/baskets/baskets_index_page_spec.rb
+++ spec/features/baskets/baskets_index_page_spec.rb
@@ -5,12 +5,26 @@ require 'rails_helper'
 RSpec.feature 'Baskets Index Page' do
   describe 'When I visit my baskets page' do
     let(:user) { create(:user, :with_baskets) }
+    let(:other_user) { create(:user, :with_baskets) }
     before { sign_in(user) }
 
     it 'should have a list of all of my baskets' do
       visit baskets_path(user)
-      save_and_open_page
       expect(page).to have_selector('.basket-row', count: user.baskets.count)
+    end
+
+    it 'should not see other users baskets' do
+      visit baskets_path(user)
+      other_user.baskets.each do |basket|
+        expect(page).to_not have_selector('.basket-row', text: basket.name)
+      end
+    end
+  end
+
+  describe 'When I visit the baskets page when not logged in' do
+    it 'will redirect me to the log in page' do
+      visit baskets_path
+      expect(current_path).to eq(new_user_session_path)
     end
   end
 end
