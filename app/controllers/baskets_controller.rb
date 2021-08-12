78f3bc64b8d0b75141779dc9fe5dc8edb9549fd4
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-12 12:13:44 -0600

    Define show create and new actions for Basket Controller
    Update controller errors and add name parameter

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -13,4 +13,42 @@ class BasketsController < ApplicationController
     flash[:alert] = "Basket was successfully deleted."
     redirect_to baskets_path
   end
+
+  def show
+    @basket = Basket.find(params[:id])
+    @receipt = Baskets::Receipt.new(@basket)
+  end
+
+  def new
+    @basket = Basket.new
+  end
+
+  def create
+    if file_type_allowed?
+      @basket = current_user.baskets.new(basket_params.except(:basket_items_file))
+      BasketReader.read_basket(basket_params[:basket_items_file]).each do |line_item|
+        @basket.line_items << line_item
+      end
+
+      if @basket.save
+        redirect_to basket_path(@basket.id)
+      else
+        flash[:error] = @basket.errors.full_messages.map(&:to_s).join(', ')
+        redirect_to new_basket_url
+      end
+    else
+      flash[:error] = "File type not allowed."
+      redirect_to new_basket_url
+    end
+  end
+
+  private
+
+  def file_type_allowed?
+    params[:basket][:basket_items_file].content_type == 'text/plain'
+  end
+
+  def basket_params
+    params.require(:basket).permit(:name, :basket_items_file)
+  end
 end
