9cbb40bb8d21da229334091ec88ccc72d798a52d
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-11 18:10:53 -0600

    Implement tests to set item category for LineItem
    Item category is an important field for the LineItem object.
    It will help us later determine if it is a taxable item or not.
    This commit implements the tests for this field that utilize two models.
    The BasketReader model which will parse the uploaded text file and the
    LineItems::ItemCategory model which will return the item category.
    Tests include the expected behavior of each model as well as a test to
    ensure that the item category is set correctly under LineItem.

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe LineItem do
   describe 'relationships' do
     it { should belong_to :basket }
   end
+
+  describe 'set_category' do
+    let(:basket) { create(:basket) }
+    it 'should set item_category column for LineItem before saving' do
+      line_item = BasketReader.line_item('1 book at 12.49')
+      expect(line_item.item_category).to eq(nil)
+      basket.line_items << line_item
+      expect(line_item.item_category).to eq('Book')
+    end
+  end
 end
