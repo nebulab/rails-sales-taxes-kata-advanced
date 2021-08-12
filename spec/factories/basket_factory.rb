b453a6907715a1d653542f6a1db485eb081ae916
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-11 23:24:08 -0600

    Make basket factory static

--- spec/factories/basket_factory.rb
+++ spec/factories/basket_factory.rb
@@ -12,13 +12,13 @@ FactoryBot.define do
     end
 
     trait :with_static_items do
-      after(create) do |basket|
-        FactoryBot.create(:line_item, tax_free: true, price: 5.50, basket: basket)
-        FactoryBot.create(:line_item, taxable: true, price: 5.50, basket: basket)
+      after(:create) do |basket|
+        FactoryBot.create(:line_item, tax_free: true, price: 10, quantity: 1, basket: basket)
+        FactoryBot.create(:line_item, taxable: true, price: 10, quantity: 1, basket: basket)
         FactoryBot.create(:line_item, taxable: true,
-                          imported: true, price: 5.50, basket: basket)
+                          imported: true, price: 10, quantity: 1, basket: basket)
         FactoryBot.create(:line_item, tax_free: true,
-                          imported: true, price: 5.50, basket: basket)
+                          imported: true, price: 10, quantity: 1, basket: basket)
       end
     end
   end
