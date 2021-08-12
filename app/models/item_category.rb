fca7abe446c10acfe513d6a2d2042c480524c8b6
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-12 14:09:13 -0600

    Revert "Basket"

--- app/models/item_category.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class ItemCategory < ApplicationRecord
-  has_many :line_items, dependent: :destroy
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-categories = ['Medicine', 'Book', 'Food', 'Others']
-
-categories.each do |category|
-  ItemCategory.find_or_create_by(name: category)
-end
-
-line_item_descriptions = {'book' => 'Book',
-                          'music CD' => 'Others',
-                          'chocolate' => 'Food',
-                          'perfume' => 'Others',
-                          'pills' => 'Medicine'
-}
-
-line_item_descriptions.each do |line_item_description, category|
-  LineItem.find_or_create_by(description: line_item_description, item_category: ItemCategory.find_by(name: category),
-                             price: 1, quantity: 1)
-end
