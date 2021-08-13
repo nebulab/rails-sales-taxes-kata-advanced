85574abf5d2e5ba9a19df83f35428cd4a0228143
Connor Ferguson <68167430+cpfergus1@users.noreply.github.com>
2021-08-13 09:00:08 -0600

    Refactor create action for baskets controller
    Refactored to improve the readability of the code. Basket_items_file
    renamed to just basket_file to make it more clear. Extracting the
    BasketHelper to add_items method works better as a model method and
    removes that logic from the controller. Relocate file_type_allowed to
    the basket helper as it should know whether the file is allowed or not.
    Changes file_type_allowed to file_type_not_allowed to work with and if
    instead of else to make it more clear.

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -3,6 +3,7 @@
 class BasketsController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user, only: [:show, :destroy]
+  before_action :check_file_type, only: [:create]
 
   def index
     @baskets = current_user.baskets.order(created_at: :desc)
@@ -25,36 +26,32 @@ class BasketsController < ApplicationController
   end
 
   def create
-    if file_type_allowed?
-      @basket = current_user.baskets.new(basket_params.except(:basket_items_file))
-      BasketReader.read_basket(basket_params[:basket_items_file]).each do |line_item|
-        @basket.line_items << line_item
-      end
+    @basket = current_user.baskets.new(basket_params.except(:basket_file))
+    @basket.add_items(basket_params[:basket_file])
 
-      if @basket.save
-        redirect_to basket_path(@basket.id)
-      else
-        flash[:error] = @basket.errors.full_messages.map(&:to_s).join(', ')
-        redirect_to new_basket_url
-      end
+    if @basket.save
+      redirect_to basket_path(@basket.id)
     else
-      flash[:error] = "File type not allowed."
+      flash[:error] = @basket.errors.full_messages.map(&:to_s).join(', ')
       redirect_to new_basket_url
     end
   end
 
   private
 
-  def file_type_allowed?
-    params[:basket][:basket_items_file].content_type == 'text/plain'
-  end
-
   def basket_params
-    params.require(:basket).permit(:name, :basket_items_file)
+    params.require(:basket).permit(:name, :basket_file)
   end
 
   def correct_user
     @basket = current_user.baskets.find_by(id: params[:id])
     redirect_to root_url if @basket.nil?
+  end
+
+  def check_file_type
+    if BasketReader.file_type_not_allowed?(basket_params[:basket_file])
+      flash[:error] = 'File type not allowed.'
+      redirect_to new_basket_path
+    end
   end
 end
